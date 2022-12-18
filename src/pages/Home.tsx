fbb804fdc1a749518ad932f19e0d800b15684e28
kegcheck <65734212+kegcheck@users.noreply.github.com>
2022-12-18 10:05:32 -0600

    initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+const Home = () => {
+    return ( 
+        <>
+            <div className="flex m-24">
+                <div className="flex-initial w-7/12">
+                    <h1>Don't know what to do with your appliances?</h1>
+                </div>
+                <div className="flex-initial bg-red-200 w-5/12">
+                    <h1>photo</h1>
+                </div>
+            </div>
+            
+        </> 
+    );
+}
+ 
+export default Home;
