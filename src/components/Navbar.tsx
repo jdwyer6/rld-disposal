5d7e1ba7f0f9048f6dcc0a7248a232e96ff67256
kegcheck <65734212+kegcheck@users.noreply.github.com>
2024-03-01 18:38:03 -0600

    update home styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,33 +14,21 @@ const Navbar = () => {
     };
 
     return ( 
-        <nav className="flex items-center justify-between flex-wrap bg-teal-600 py-4 px-24">
-            <Link to='/' className="flex items-center flex-shrink-0 text-white mr-6">
-                <img src={logo} width='24' className='mr-3'/>
-                <span className="font-semibold text-xl tracking-tight brand-text">RLD Disposal</span>
-            </Link>
-            <div className="block lg:hidden">
-                <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-                </button>
-            </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                <div className="text-sm lg:flex-grow">
-                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-                    Home
-                </a>
-                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-                    About
-                </a>
-                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white">
-                    Schedule service
-                </a>
-                </div>
-                <div>
-                <a href="#" onClick={openModal} className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Login</a>
-                </div>
+            <nav className="container-fluid">
+                <ul>
+                    <Link to='/' className="flex items-center flex-shrink-0 text-white mr-6">
+                        <img src={logo} width='24' className='mr-3'/>
+                        <span className="font-semibold text-xl tracking-tight brand-text">RLD Disposal</span>
+                    </Link>
+                </ul>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">About</a></li>
+                    <li><a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white">Schedule service</a></li>
+                    <li><a href="#" onClick={openModal} className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Login</a></li>
+                </ul>
+                
                 {showModal && <LoginModal onClose={closeModal} />}
-            </div>
             </nav>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
-import homePhoto from '../images/home_image.jpg';
-import bgApplianceImage from '../images/bg-appliances.png';
+import homePhoto from '../images/kitchen.jpg';
 import cardSchedule from '../images/cardSchedule.jpg';
 import cardLearnMore from '../images/cardLearnMore.jpg';
 import {TbHomeOff, TbTrash, TbTruckDelivery} from 'react-icons/tb';
@@ -9,62 +8,64 @@ import {Link} from 'react-router-dom';
 const Home = () => {
 
     return ( 
-        <div className="relative">
-            <img src={bgApplianceImage} alt="Appliances" className="bg-image" />
-            <div className="container my-4">
-                <div className="grid grid-flow-col">
-                <div>
-                    <h1>Don't know what to do with your appliances?</h1>
-                    <p>Contact RLD Disposal today for quality appliance delivery, installation, removal, and more from technicians who care.</p>
-                    <div className="my-2">
-                    <Link to="/schedule">
-                        <button className="button primary">Make an appointment</button>
-                    </Link>
-                    <button className="button">Learn More</button>
+        <div className="flex flex-col">
+
+
+            <div className="hero overlay flex align-center justify-center">
+                <div className="content w-50">
+                    <h1 className="text-white text-center hero-text">Don't know what to do with your appliances?</h1>
+                    <h4 className="text-white text-center">Contact RLD Disposal today for quality appliance delivery, installation, removal, and more from technicians who care.</h4>
+                    <div className="flex justify-center">
+                        <Link to="/schedule">
+                            <button>Make an appointment</button>
+                        </Link>
                     </div>
                 </div>
-                <div className="flex justify-center">
-                    <img className="responsive-image" src={homePhoto} alt="Home Appliances" />
-                </div>
-                </div>
             </div>
+            
 
-            <div className="bg-primary text-white text-center py-6">
-                <h1>We address all your appliance annoyances.</h1>
-                <div className="grid grid-flow-col gap-4">
-                <div className="text-center">
-                    <TbHomeOff className="icon-large" />
-                    <p>Big box stores deliver but won't install?</p>
-                </div>
-                <div className="text-center">
-                    <TbTrash className="icon-large" />
-                    <p>Old appliances gotta go?</p>
-                </div>
-                <div className="text-center">
-                    <TbTruckDelivery className="icon-large" />
-                    <p>Bought an appliance but have no one to deliver?</p>
-                </div>
-                </div>
-            </div>
+ 
+            
 
-            <div className="container my-6">
-                <h2 className="text-center my-9">Serving the greater St. Louis area since 1953.</h2>
-                <div className="grid grid-flow-col gap-4">
-                <Card
-                    title="Schedule an appointment online"
-                    subtitle="Fast, easy and convenient. Pick a time that's right for you and pay online or in-person."
-                    image={cardSchedule}
-                    btnText="Get started"
-                />
-                <Card
-                    title="Decades of dedication and experience."
-                    subtitle="With over 70 years of satisfied customers, we guarantee you are getting the best service around."
-                    image={cardLearnMore}
-                    btnText="Learn more"
-                />
+    
+
+  
+
+
+                <div className="bg-primary text-white text-center py-6">
+                    <h1>We address all your appliance annoyances.</h1>
+                    <div className="text-center">
+                        <TbHomeOff className="icon-large" />
+                        <p>Big box stores deliver but won't install?</p>
+                    </div>
+                    <div className="text-center">
+                        <TbTrash className="icon-large" />
+                        <p>Old appliances gotta go?</p>
+                    </div>
+                    <div className="text-center">
+                        <TbTruckDelivery className="icon-large" />
+                        <p>Bought an appliance but have no one to deliver?</p>
+                    </div>
                 </div>
-            </div>
+
+                <div className="container my-6">
+                    <h2 className="text-center my-9">Serving the greater St. Louis area since 1953.</h2>
+                    <Card
+                        title="Schedule an appointment online"
+                        subtitle="Fast, easy and convenient. Pick a time that's right for you and pay online or in-person."
+                        image={cardSchedule}
+                        btnText="Get started"
+                    />
+                    <Card
+                        title="Decades of dedication and experience."
+                        subtitle="With over 70 years of satisfied customers, we guarantee you are getting the best service around."
+                        image={cardLearnMore}
+                        btnText="Learn more"
+                    />
+                </div>
+  
         </div>
+        
     );
 }
  
