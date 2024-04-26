ab544896f5806469cc92bde5c3226fdbf42d18eb
kegcheck <65734212+kegcheck@users.noreply.github.com>
2024-04-26 12:41:09 -0500

    add services page

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -4,13 +4,19 @@ import cardLearnMore from '../images/cardLearnMore.jpg';
 import {TbHomeOff, TbTrash, TbTruckDelivery} from 'react-icons/tb';
 import Card from '../components/card';
 import {Link} from 'react-router-dom';
+import installPhoto from '../images/install.jpg';
+import movePhoto from '../images/move.jpg';
 
 const Services = () => {
 
     return ( 
         <div className="flex flex-col">
-            <h1>services</h1>
-  
+            <h1 className="text-center">Services</h1>
+            <div>
+            <Card title="Haul away an appliance" subtitle="Starting at $19" image={movePhoto} btnText="Schedule"/>
+            <Card title="Install an appliance" subtitle="Starting at $49" image={installPhoto} btnText="Schedule"/>
+            </div>
+            
         </div>
         
     );
